0596caeb46b60df47e5bd5d0d025a1b649a236a7
RubyRed <77550437+AndreyYad@users.noreply.github.com>
2024-03-01 02:02:04 +0300

    Update dispatcher.py

--- bot/dispatcher.py
+++ bot/dispatcher.py
@@ -3,9 +3,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from handlers import generic
 
-storage = MemoryStorage()
-
-dp = Dispatcher(storage=storage)
+dp = Dispatcher()
 
 dp.include_routers(
     generic.router
