413fe5882c535a6fc497b4c8a1df34905e64247e
RubyRed <77550437+AndreyYad@users.noreply.github.com>
2024-03-15 14:17:27 +0300

    Update text.py

--- modules/text.py
+++ modules/text.py
@@ -1,6 +1,6 @@
 class Text:
     
-    start_text = 'Здравствуй, товарищ!\n\nЗаргестрирую почту на Proton Mail и скинь боту её адрес, что бы он её сохранил.\nЗарегестрироваться в Proton можно <a href="https://account.proton.me/mail/signup?plan=free&billing=12&minimumCycle=12&currency=EUR&ref=prctbl">здесь</a>.\n\nИ желательно поставьте партийный ВПН на случай блокировки Proton и просто обывательной нужды, как это сделать написано <a href="https://t.me/c/1961813443/21128">тут</a>.'
+    start_text = 'Здравствуй, соучредитель!\n\nЗаргестрирую почту на Proton Mail и скинь боту её адрес, что бы он её сохранил.\nЗарегестрироваться в Proton можно <a href="https://account.proton.me/mail/signup?plan=free&billing=12&minimumCycle=12&currency=EUR&ref=prctbl">здесь.</a>\n\nИ желательно поставьте партийный ВПН на случай блокировки Proton и просто обывательной нужды, как это сделать написано <a href="https://t.me/c/1961813443/21128">тут.</a>'
 
     email_save = 'Почта сохранена!'
 
